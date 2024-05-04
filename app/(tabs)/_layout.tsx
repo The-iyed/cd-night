516b5ffc10c76838cf30d15645d8972b3ab08a93
mohamed <mbenchikha5@gmail.com>
2024-05-04 21:43:04 +0100

    feat(first commit): first

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -6,7 +6,6 @@ import Colors from '@/constants/Colors';
 import {useColorScheme} from '@/components/useColorScheme';
 import {useClientOnlyValue} from '@/components/useClientOnlyValue';
 
-// You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
 function TabBarIcon(props: {
     name: React.ComponentProps<typeof FontAwesome>['name'];
     color: string;
@@ -21,45 +20,36 @@ export default function TabLayout() {
         <Tabs
             screenOptions={{
                 tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-                // Disable the static render of the header on web
-                // to prevent a hydration error in React Navigation v6.
                 headerShown: useClientOnlyValue(false, true),
             }}>
             <Tabs.Screen
                 name="index"
                 options={{
-                    title: 'Tab One',
+                    title: 'Youssef',
                     tabBarIcon: ({color}) => <TabBarIcon name="code" color={color}/>,
-                    headerRight: () => (
-                        <Link href="/modal" asChild>
-                            <Pressable>
-                                {({pressed}) => (
-                                    <FontAwesome
-                                        name="info-circle"
-                                        size={25}
-                                        color={Colors[colorScheme ?? 'light'].text}
-                                        style={{marginRight: 15, opacity: pressed ? 0.5 : 1}}
-                                    />
-                                )}
-                            </Pressable>
-                        </Link>
-                    ),
                 }}
             />
             <Tabs.Screen
                 name="two"
                 options={{
-                    title: 'Tab Two',
+                    title: 'Iyed',
                     tabBarIcon: ({color}) => <TabBarIcon name="code" color={color}/>,
                 }}
             />
             <Tabs.Screen
                 name="posts"
                 options={{
-                    title:'Posts',
+                    title: 'Mohamed',
                     tabBarIcon: ({color}) => <TabBarIcon name="code" color={color}/>,
                 }}
-                />
+            />
+            <Tabs.Screen
+                name="home"
+                options={{
+                    title: 'Loay',
+                    tabBarIcon: ({color}) => <TabBarIcon name="code" color={color}/>,
+                }}
+            />
         </Tabs>
     );
 }
