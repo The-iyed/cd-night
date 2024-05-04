867624571d7b962c624a735103b2855382957e4a
mohamed <mbenchikha5@gmail.com>
2024-05-04 22:41:20 +0100

    feat(chat gpt ai): add api

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,7 +2,6 @@ import { StyleSheet } from 'react-native';
 
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
-import ImagePickerExample from '@/components/ImagePicker';
 
 export default function TabOneScreen() {
   return (
@@ -10,7 +9,6 @@ export default function TabOneScreen() {
       <Text style={styles.title}>Tab One</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="app/(tabs)/index.tsx" />
-      <ImagePickerExample/>
     </View>
   );
 }
--- app/(tabs)/posts.tsx
+++ app/(tabs)/posts.tsx
@@ -1,9 +1,92 @@
-import { View } from '@/components/Themed';
+import {View} from '@/components/Themed';
 import {Button} from 'react-native';
+import {Text} from 'tamagui';
+import {ImagePickerExample} from '@/components/ImagePicker';
+import {useState} from 'react';
+import axios from 'axios';
+import {ImagePickerAsset} from 'expo-image-picker/src/ImagePicker.types';
 
-export default () => {
-    return <View>
-        <Button title={"Create Your Post"}/>
-        Hello this is the section for posts
-    </View>
+export default function Posts() {
+    const [image, setImage] = useState<ImagePickerAsset | null>(null);
+
+    return (
+        <View style={{flex: 1}}>
+            <Button title={"Create Your Post"} onPress={async () => {
+                if (image && image.base64 && image.mimeType) {
+                    const response = await callChatGpt(image.base64, image.mimeType);
+                }
+            }}/>
+            <Text>{process.env?.EXPO_PUBLIC_API_KEY}</Text>
+            <ImagePickerExample image={image} setImage={setImage}/>
+        </View>
+    )
 }
+
+const headers = {
+    "Content-Type": "application/json",
+    "Authorization": `Bearer ${process.env.EXPO_PUBLIC_API_KEY}`
+}
+
+const callChatGpt = async (uri: string, mimetype: string) => {
+    console.log(process.env.EXPO_PUBLIC_API_KEY)
+    return axios.post<ChatCompletion>(
+        'https://api.openai.com/v1/chat/completions',
+        {
+            model: "gpt-4-turbo",
+            messages: [
+                {
+                    role: 'system',
+                    content:
+                        'you are an assistant that give simple answers questions. no longer than 50 charters',
+                },
+                {
+                    role: "user",
+                    content: [
+                        {
+                            "type": "text",
+                            "text": "What’s in this image?"
+                        },
+                        {
+                            "type": "image_url",
+                            "image_url": {
+                                "url": `data:image/${mimetype};base64,${uri}`
+                            }
+                        },
+                    ]
+                }
+
+            ],
+        }, {
+            headers
+        })
+        .then((m) => {
+            console.log(m.data)
+            console.log(m.data.choices)
+        }).catch(e => {
+            console.log(e?.message)
+        });
+}
+
+type ChatCompletion = {
+    id: string;
+    object: string;
+    created: number;
+    model: string;
+    choices: Choice[];
+    usage: {
+        prompt_tokens: number;
+        completion_tokens: number;
+        total_tokens: number;
+    };
+    system_fingerprint: string;
+};
+
+type Choice = {
+    index: number;
+    message: {
+        role: string;
+        content: string;
+    };
+    logprobs: null | any;
+    finish_reason: string;
+};
