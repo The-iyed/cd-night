0ad15f75bc35e5466e349e36ec52113b3174defa
louay24 <loueyzouaoui@takiacademyteam.com>
2024-05-05 02:44:30 +0100

    mock data

--- data/posts.ts
+++ data/posts.ts
@@ -1,8 +1,8 @@
 import { findUser } from ".";
-
 export const posts = [
   {
-    videoSrc: "https://www.youtube.com/watch?v=tLULIzOj-Ew",
+    videoSrc:
+      "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
     thumbnailSrc:
       "https://i.ytimg.com/vi/0OGIQXortBk/oar2.jpg?sqp=-oaymwEdCJUDENAFSFWQAgHyq4qpAwwIARUAAIhCcAHAAQY=&rs=AOn4CLCg1AqEhMZHxPJjfT0YJ_8jpHgWgg",
     description: "شنـــوّا نقصدو بعبـــــارة a frog in your throat 🐸",
@@ -11,7 +11,8 @@ export const posts = [
     tags: ["english", "learning"],
   },
   {
-    videoSrc: "https://www.youtube.com/shorts/0OGIQXortBk?feature=share",
+    videoSrc:
+      "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4",
     thumbnailSrc:
       "https://i.ytimg.com/vi/0OGIQXortBk/oar2.jpg?sqp=-oaymwEdCJUDENAFSFWQAgHyq4qpAwwIARUAAIhCcAHAAQY=&rs=AOn4CLCg1AqEhMZHxPJjfT0YJ_8jpHgWgg",
     description: "شنـــوّا نقصدو بعبـــــارة a frog in your throat 🐸",
@@ -20,7 +21,8 @@ export const posts = [
     tags: ["english", "learning"],
   },
   {
-    videoSrc: "https://www.youtube.com/shorts/Bs3HTza_Rvc?feature=share",
+    videoSrc:
+      "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4",
     thumbnailSrc:
       "https://i.ytimg.com/vi/Bs3HTza_Rvc/hq720.jpg?sqp=-oaymwE2CNAFEJQDSFXyq4qpAygIARUAAIhCGABwAcABBvABAfgB_gSAAvAIigIMCAAQARhSIF0oZTAP&rs=AOn4CLBFGn_0itqDOqo6UseCZ4IjahXKcg",
     description: " غلطة تنقص في معدلك في الباك !",
@@ -29,7 +31,8 @@ export const posts = [
     tags: ["bac sc", "bac math", "bac info"],
   },
   {
-    videoSrc: "https://www.youtube.com/shorts/VvpvwyCRN1Y?feature=share",
+    videoSrc:
+      "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4",
     thumbnailSrc:
       "https://i.ytimg.com/vi/VvpvwyCRN1Y/hq720_2.jpg?sqp=-oaymwE2CNAFEJQDSFXyq4qpAygIARUAAIhCGABwAcABBvABAfgBtgiAAoAPigIMCAAQARg8IEoocjAP&rs=AOn4CLBvz4p8sg7a51OL0STuPUPJQY1vRw",
     description: "METTRE K.O. LE BAC 👊",
@@ -38,16 +41,18 @@ export const posts = [
     tags: ["mathematics"],
   },
   {
-    videoSrc: "https://www.youtube.com/shorts/IuOLMNX9zaU?feature=share",
+    videoSrc:
+      "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
     thumbnailSrc:
-      "https://i.ytimg.com/vi/IuOLMNX9zaU/hq720_2.jpg?sqp=-oaymwE2CNAFEJQDSFXyq4qpAygIARUAAIhCGABwAcABBvABAfgBzgWAAoAKigIMCAAQARh_IEgoLjAP&rs=AOn4CLD-af_rri0Sp6f7TJrPxFueiDklCw",
+      "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4",
     description: "Real-life Application of Calculus",
     author: findUser("0004"),
     name: "Math Integration Timelapse ",
     tags: ["mathematics"],
   },
   {
-    videoSrc: "https://www.youtube.com/shorts/M_TsSDDycg4?feature=share",
+    videoSrc:
+      "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
     thumbnailSrc:
       "https://i.ytimg.com/vi/M_TsSDDycg4/oardefault.jpg?sqp=-oaymwEdCJUDENAFSFWQAgHyq4qpAwwIARUAAIhCcAHAAQY=&rs=AOn4CLB1Jn6gHEK5D3b9Fffl9bNA-EqyLg",
     description: "تطويرالذات",
@@ -56,7 +61,8 @@ export const posts = [
     tags: ["تطويرالذات"],
   },
   {
-    videoSrc: "https://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4",
+    videoSrc:
+      "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
     thumbnailSrc:
       "https://marketplace.canva.com/EAFf5rfnPgA/1/0/1600w/canva-blue-modern-eye-catching-vlog-youtube-thumbnail-LEcp-BYepDU.jpg",
     description: "ssssss",
